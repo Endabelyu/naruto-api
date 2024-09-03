7be07a22227d9d63f65093d17a87e683b5e28fe2
naswa <endabelyuproject@gmail.com>
2024-09-03 21:28:55 +0700

    feat(family.schema): create family schema, create family schema, create conflict data schema

---
+++ src/schemas/family.schema.ts
@@ -0,0 +1,40 @@
+import { z } from '@hono/zod-openapi';
+export const IdSchema = z.coerce.number().int().min(1).openapi({
+  description: 'Unique identifier for the clan',
+  example: 1,
+});
+
+// Define the Zod schema for the Family type
+export const FamilySchema = z
+  .object({
+    id: IdSchema,
+    name: z.string().min(1, 'Name is Required').openapi({
+      description: 'The name of ninja family',
+      example: 'Minato',
+    }),
+    created_at: z.date().optional().openapi({
+      description: 'Timestamp when the data family was created',
+      example: '2023-08-24T12:00:00Z',
+    }),
+    updated_at: z.date().optional().openapi({
+      description: 'Timestamp when the family was last updated',
+      example: '2023-08-25T12:00:00Z',
+    }),
+  })
+  .openapi('Family');
+
+export const createFamilySchema = FamilySchema.omit({
+  id: true,
+  created_at: true,
+  updated_at: true,
+}).openapi('CreateFamily');
+
+export const createFamilyConflictData = z.object({
+  ok: z.boolean().openapi({
+    example: false,
+  }),
+  message: z.string().openapi({
+    description: 'Message according error',
+    example: 'Data family already exist!',
+  }),
+});
